099a862bb78be95d450fa45c326adc3759dd4c62
Rustem <davletov.rustem@mail.ru>
2025-11-22 18:58:25 +0500

    refactoring

--- tests/pages/SubscriptionsPage.ts
+++ tests/pages/SubscriptionsPage.ts
@@ -11,7 +11,7 @@ export class SubscriptionsPage extends BasePage {
   }
 
   async open() {
-    await this.page.goto('https://rutube.ru/my/subscriptions/');
+    await this.page.goto('/my/subscriptions/');
   }
 
   async contentHasCorrectAriaSnapshot() {
